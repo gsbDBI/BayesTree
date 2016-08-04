ed1ab0c14adadfe9462b2cc868bab63ff36398e6
vikas84bf <vikas.ramachandra@gmail.com>
2016-08-04 10:50:18 -0700

    .

--- src/MuS.cpp
+++ src/MuS.cpp
@@ -40,7 +40,7 @@ void MuS::updatepost()
      {
       for(i=1;i<=nob;i++)
       {
-        printf("Inside updatepost, weights_flag=%d\n",weights_flag);
+        Rprintf("Inside updatepost, weights_flag=%d\n",weights_flag);
         //if(weights_flag)
         //{
         ybar += y[indices[i]]*w[indices[i]];
