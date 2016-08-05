b48b8f79facefc6d0a916b57c195a5ef84d6f6f3
vikas84bf <vikas.ramachandra@gmail.com>
2016-08-04 17:39:18 -0700

    .

--- src/MuS.cpp
+++ src/MuS.cpp
@@ -82,7 +82,7 @@ void MuS::updatepost()
    }
    //FILE *fptr;
    //fptr=fopen("test_dump.txt","a");
-   //Rprintf("nob=%d,ybar=%f,sumweights=%f\n",nob,ybar,sumweights);
+   Rprintf("nob=%d,ybar=%f,sumweights=%f\n",nob,ybar,sumweights);
    //fprintf(fptr,"nob=%d,ybar=%f,sumweights=%f\n",nob,ybar,sumweights);
    //fclose(fptr);
    ////free(fptr);
